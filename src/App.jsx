46065414b90de0993b9f188c2e8e21273e87123b
mohammed ali <ak3757488@gmail.com>
2025-04-07 15:30:48 +0530

    date changes

--- src/App.jsx
+++ src/App.jsx
@@ -53,11 +53,11 @@ const ProjectPageLayout = () => (
       <center>
         <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
         <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
-          © 2023{" "}
-          <a href="https://github.com/Mohad-Ali" className="hover:underline">
-            Mohad-Ali™
-          </a>
-          . All Rights Reserved.
+        © {new Date().getFullYear()}{" "}
+                <a href="https://github.com/Mohad-Ali" className="hover:underline">
+                  Mohad-Ali™
+                </a>
+                . All Rights Reserved.
         </span>
       </center>
     </footer>
