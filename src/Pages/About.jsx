b06853488d978bd4063df5c0d6adf357bab47f63
mohammed ali <ak3757488@gmail.com>
2025-04-07 15:43:12 +0530

    spell changes

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -229,7 +229,7 @@ and database management using MongoDB.
                 <FileText className="w-4 h-4 sm:w-5 sm:h-5" /> Download CV
               </button>
               </a>
-              <a href="#Portofolio" className="w-full lg:w-auto">
+              <a href="#Portfolio" className="w-full lg:w-auto">
               <button 
                 data-aos="fade-up"
                 data-aos-duration="1000"
@@ -244,7 +244,7 @@ and database management using MongoDB.
           <ProfileImage />
         </div>
 
-        <a href="#Portofolio">
+        <a href="#Portfolio">
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-16 cursor-pointer">
             {statsData.map((stat) => (
               <StatCard key={stat.label} {...stat} />
